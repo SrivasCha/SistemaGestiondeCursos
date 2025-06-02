fed91b2e330a3d228247e6b06314bd5a41122164
Sebastian Sanabria <jhonsanabria407@gmail.com>
2025-06-02 01:46:24 -0500

    se modifico el app.jsx para que haya seguridad en los paneles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,27 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login/Login';
 import PanelAdmin from './components/admin/PanelAdmin';
 import PanelProfesor from './components/profesor/PanelProfesor';
 import PanelEstudiante from './components/estudiante/PanelEstudiante';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/admin" element={<PanelAdmin />} />
-        <Route path="/profesor" element={<PanelProfesor />} />
-        <Route path="/estudiante" element={<PanelEstudiante />} />
+        <Route path="/login" element={<Login />} />
+        {/* Rutas protegidas */}
+        <Route element={<ProtectedRoute />}>
+          <Route path="/admin" element={<PanelAdmin />} />
+          <Route path="/profesor" element={<PanelProfesor />} />
+          <Route path="/estudiante" element={<PanelEstudiante />} />
+        </Route>
+        {/* Redirección por defecto */}
+        <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
+
